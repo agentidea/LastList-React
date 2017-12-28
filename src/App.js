e06126299e16b114137e6b25c4c56491f0354636
Valentin Hervieu <valentin@hervi.eu>
2017-12-28 13:11:34 +0100

    Implement Login screen

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { loggedInSelector } from './common/state/user/selectors'
+import * as userActionCreators from './common/state/user/actions'
+import Background from './common/components/Background'
+import Header from './common/components/Header'
+import Footer from './common/components/Footer'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import styles from './App.module.css'
+
+const mapStateToProps = state => ({
+  isLoggedIn: loggedInSelector(state),
+})
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(userActionCreators, dispatch),
+})
+
+class App extends Component {
+  componentDidMount() {
+    if (this.props.isLoggedIn) {
+      this.props.userActions.getCurrentUser()
+    }
+  }
+
+  render() {
+    const { isLoggedIn } = this.props
+    return (
+      <Router>
+        <div className={styles.app}>
+          <Background />
+          <div className={styles.main}>
+            <Header isLoggedIn={isLoggedIn} />
+            <div className={styles.content}>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,119 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { FormControlLabel } from 'material-ui/Form'
+import Checkbox from 'material-ui/Checkbox'
+
+import * as userActionCreators from '../../common/state/user/actions'
+import UserError from '../../common/state/user/error'
+import Button from '../../common/components/Button'
+import Textfield from '../../common/components/Textfield'
+import styles from './Login.module.css'
+
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(userActionCreators, dispatch),
+})
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    remember: true,
+    error: {
+      field: null,
+      message: null,
+    },
+  }
+
+  componentWillMount() {
+    this.props.userActions.signOut()
+  }
+
+  onChange = (field, value) => {
+    this.setState({ [field]: value, error: { field: null } })
+  }
+
+  // On successful login, redirect the user to the intended page.
+  redirect() {
+    const { location, history } = this.props
+    const match = location.pathname.match(/[?|&]fwd=\/?([-%\d\w]+)/)
+    const fwd = (match && match[1]) || ''
+    history.push(`/${fwd}`)
+  }
+
+  onSubmit = event => {
+    event.preventDefault()
+    const { email, password } = this.state
+    this.props.userActions
+      .login(email, password)
+      .then(resp => {
+        this.redirect()
+      })
+      .catch(error => {
+        if (error instanceof UserError) {
+          const { field, message } = error
+          this.setState({ error: { field, message } })
+        } else {
+          this.setState({ error: { field: 'email', message: 'Unknown error' } })
+        }
+      })
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form className={styles.login} onSubmit={this.onSubmit}>
+        <h3>Sign In To Your Last List</h3>
+        {this.renderInputs()}
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.remember}
+              onChange={(e, checked) => this.setState({ remember: checked })}
+            />
+          }
+          label="Remember me"
+        />
+        <div className={styles.buttons}>
+          <Button to="/signup" nonprimary>
+            Create Account
+          </Button>
+          <Button className={styles.loginButton}>Sign In</Button>
+        </div>
+        <p>
+          <Link to="/forgot">Forgot password?</Link>
+        </p>
+      </form>
+    )
+  }
+
+  renderInputs() {
+    const { error, email, password } = this.state
+    const errorEmail = error.field === 'email' ? error.message : null
+    const errorPw = error.field === 'password' ? error.message : null
+    return (
+      <div>
+        <Textfield
+          type="email"
+          label="Email Address"
+          value={email}
+          required
+          error={errorEmail}
+          placeholder="Your email address"
+          onInput={value => this.onChange('email', value)}
+        />
+        <Textfield
+          type="password"
+          label="Password"
+          value={password}
+          error={errorPw}
+          placeholder="Your password"
+          required
+          onInput={value => this.onChange('password', value)}
+        />
+      </div>
+    )
   }
 }
-export default Login;
+
+export default connect(null, mapDispatchToProps)(Login)
