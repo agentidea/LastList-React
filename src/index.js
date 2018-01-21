72939724aa09ef00864c1591b40a9dac3ebcf6ae
Valentin Hervieu <valentin@hervi.eu>
2018-01-21 16:34:04 +0100

    Update to latest material-ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { createMuiTheme, MuiThemeProvider } from 'material-ui/styles'
 
 import App from './App'
 import createStore from './store'
@@ -9,9 +10,23 @@ import './global.css'
 
 const store = createStore()
 
+
+const mainColor = '#3917fe'
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: mainColor,
+      // we need to use this legacy theme until material-ui-pickers is updated to latest material-ui
+      500: mainColor
+    }
+  }
+});
+
 const AppWithProviders = () => (
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>
 )
 
