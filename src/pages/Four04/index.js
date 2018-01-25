7b6e134bb41b21b4ab79c916565d42f270f25bd0
Valentin Hervieu <valentin@hervi.eu>
2018-01-25 19:01:56 +0100

    Fix 404 page design

--- src/pages/Four04/index.js
+++ src/pages/Four04/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Button from '../../common/components/Button'
+import styles from './Four04.module.css'
 
 const Four04 = () => {
   return (
-    <div>
+    <div className={styles.center}>
       <h3>Page not found</h3>
       <p>This page doesn't exist.</p>
       <Button to="/">Go back home</Button>
