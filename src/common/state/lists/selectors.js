aedf7291b39a21779a935e78560d76361763ecfd
Valentin Hervieu <valentin@hervi.eu>
2018-01-31 21:41:16 +0100

    EditLastList: only show the addMore button when all items are filled.

--- src/common/state/lists/selectors.js
+++ src/common/state/lists/selectors.js
@@ -1,7 +1,9 @@
 /* This only keep list item with non-empty data */
 export const listsForServerSelector = state => {
   const lists = state.lists.lists
-  return lists.map(list => {
-    return list.filter(item => item.artistName !== '' || item.songName !== '')
-  })
+  return lists
+    .map(list => {
+      return list.filter(item => item.artistName !== '' || item.songName !== '')
+    })
+    .filter(list => list.length > 0)
 }
