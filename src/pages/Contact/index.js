86f5d4cae7628cf71295a8edf2b1eab43ffbf92d
Grant Steinfeld <grantsteinfeld@gmail.com>
2018-02-01 12:16:33 -0500

    some copy formatting changes

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -7,7 +7,7 @@ const Contact = () => {
       <h3>Contact Us</h3>
       <p>
         If you have any questions, please take a look at our <Link to="/faq">FAQs</Link> or email us
-        at <a href="mailto:grantsteinfeld@gmail.com">@lastlist</a>.
+        at <a href="mailto:contact@lastlist.com?subject=LastList%20Contact%20Request">contact@lastlist.com</a>.
       </p>
     </div>
   )
