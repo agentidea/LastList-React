06b090a682e015e370652ecaca2fdd43877e444e
Grant Steinfeld <grantsteinfeld@gmail.com>
2018-02-02 16:29:37 -0500

    registration flow gets the large Guaridans folder

---
+++ src/pages/Registration/Guardians-step-3/AddGuardian/index.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import * as guardiansActionCreators from '../../../../common/state/guardians/actions'
+import UserError from '../../../../common/state/user/error'
+import Button from '../../../../common/components/Button'
+import Textfield from '../../../../common/components/Textfield'
+import styles from './AddGuardian.module.css'
+
+const mapDispatchToProps = dispatch => ({
+  guardiansActions: bindActionCreators(guardiansActionCreators, dispatch),
+})
+
+class AddGuardian extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    adding: false,
+    error: null,
+    gonext: false,
+  }
+
+  goNext = () => {
+    //$to do: auto-save / prompt if dirty
+    const { history } = this.props
+    history.push('/reg/payment')
+  }
+
+  shouldShowNextButton = () => {
+    //if saved how do we tell?
+    //look in state???+
+    return true
+  }
+
+
+  render() {
+    const { firstName, lastName, email, adding, gonext, error } = this.state
+    return (
+      
+      <h3>Add Guardians</h3>
+
+      <form className={styles.addGuardian} onSubmit={this.addGuardian}>
+        <div className={styles.inputs}>
+          <Textfield
+            type="text"
+            label="First name"
+            value={firstName}
+            required
+            placeholder="Her first name"
+            onChange={value => this.onInputChange('firstName', value)}
+          />
+          <Textfield
+            type="text"
+            label="Last name"
+            value={lastName}
+            required
+            placeholder="Her last name"
+            onChange={value => this.onInputChange('lastName', value)}
+          />
+          <Textfield
+            type="email"
+            label="Email"
+            value={email}
+            required
+            placeholder="Her email address"
+            onChange={value => this.onInputChange('email', value)}
+          />
+        </div>
+        
+        {error && <div className={styles.error}>{error}</div>}
+        
+        <div className={styles.buttons} >
+        <Button>{adding ? 'Adding...' : 'Add'}</Button>
+
+        {this.shouldShowNextButton() && <Button className={styles.saveBtn} onClick={this.goNext} disabled={goingnext}>
+        {goingnext ? 'next...' : 'Next'}
+        </Button>}
+        </div>
+      </form>
+    )
+  }
+
+  onInputChange(field, value) {
+    this.setState({ [field]: value })
+  }
+
+  addGuardian = e => {
+    e.preventDefault()
+    const { firstName, lastName, email } = this.state
+    this.setState({ adding: true, error: null })
+
+    this.props.guardiansActions
+      .addGuardian(firstName, lastName, email)
+      .then(() => {
+        this.setState({
+          firstName: '',
+          lastName: '',
+          email: '',
+          adding: false,
+        })
+      })
+      .catch(error => {
+        if (error instanceof UserError) {
+          const { message } = error
+          this.setState({ error: message, adding: false })
+        } else {
+          this.setState({ error: 'Unknown error', adding: false })
+        }
+      })
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddGuardian)
---
+++ src/pages/Registration/Guardians-step-3/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import requireLogin from '../../../common/hocs/requireLogin'
+import * as guardiansActionCreators from '../../../common/state/guardians/actions'
+import Loading from '../../../common/components/Loading'
+import styles from './Guardians.module.css'
+import GuardiansList from './GuardiansList'
+import AddGuardian from './AddGuardian'
+
+const mapStateToProps = state => ({
+  guardians: state.guardians.guardians,
+  loading: state.guardians.loading,
+})
+const mapDispatchToProps = dispatch => ({
+  guardiansActions: bindActionCreators(guardiansActionCreators, dispatch),
+})
+
+class RegGuardians extends Component {
+  componentDidMount() {
+    this.props.guardiansActions.fetchUserGuardians()
+  }
+
+  render() {
+    const { loading, guardians } = this.props
+    return (
+      <div className={styles.content}>
+        <h4>Step Three</h4>
+        <h3>Nominate your guardians</h3>
+        <p>
+          Your Guardian(s) will be responsible for getting your Last List from us when the time has
+          come. We’ll let them know via email that they’ve been chosen by you to fulfill this
+          important role. You can add up to 5 Guardians and change them as many times as you want
+          to.
+        </p>
+        <h4>Your guardians</h4>
+        {loading ? (
+          <div className={styles.loaderContainer}>
+            <Loading />
+          </div>
+        ) : guardians.length === 0 ? (
+          <p>You don't have guardians at the moment</p>
+        ) : (
+          <GuardiansList
+            guardians={guardians}
+            onRemoveGuardian={uuid => this.props.guardiansActions.removeGuardian(uuid)}
+          />
+        )}
+        <h4>Add a guardian</h4>
+        <AddGuardian />
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(requireLogin(Guardians))
