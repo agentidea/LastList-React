8c3d5094ac7610d530cbd91e5ae30cb13b2ffd08
Grant Steinfeld <grantsteinfeld@gmail.com>
2018-02-11 22:39:33 -0500

    policy updated

--- src/pages/Policies/index.js
+++ src/pages/Policies/index.js
@@ -7,43 +7,43 @@ const Policies = () => {
 
       <h4>PRIVACY POLICY</h4>
       <pre>
-        1. This Privacy Policy describes what information Last List, LLC (“Last List”) collects and
-        how it is used and shared. Note that any capitalized terms not defined in this Privacy
-        Policy have the meanings set forth in Last List’s Terms of Service. If you don’t agree with
-        the terms of this Privacy Policy, you may not access or use the Services. 2. Last List is
-        committed to protecting and respecting your privacy. This policy (sets out the basis on
-        which any personal data Last List collect from you, or that you provide to us, will be
-        processed by us. Please read the following carefully to understand Last List’s views and
-        practices regarding your personal data and how Last List will treat it. By visiting
-        [www.mylastlist.com] (“Our Site”) you are accepting and consenting to the practices
-        described in this policy. 3. Last List will collect and process the following data about
-        you: a. Account Information. This may include your name, address, email address and phone
-        number. Last List use this information in the ways you would expect, such as to set up your
-        Account or contact you. b. Third-Party Account Information. If you use Third-Party Services,
-        you may provide Last List with your Third-Party Services account information, such as your
-        username (note that Last List will not store any passwords you use to access Third-Party
-        Services). Last List transmit, and may store, such account information, only as needed to
-        provide the Services, and only in accordance with the terms and policies of the Third-Party
-        Services. c. Payment Information. When you initially provide or update your payment
-        information, Last List transmit it via an encrypted connection to Last List’s Payment
-        Processor(s), which will uses and processes your payment information in accordance with
-        their policies Last List will not store your payment information, other than your zip code
-        and country, which Last List require for billing and to comply with tax and other government
-        regulations. d. Communications With Last List. When you send Last List emails or other
-        communications, such as customer support inquiries, Last List maintain those communications
-        and their contents so that Last List can resolve your inquiries or otherwise assist you. e.
-        Files You Provide Last List. When you use the Services, Last List store, process and
-        transmit Your Content. Last List may process and store such files and information in order
-        to provide the Services, as described in Last List’s Terms of Service f. Usage Information.
-        This includes information about your activity on and interaction with the Services, such as
-        your IP address, your device or browser type, the site you visited before coming to Our
-        Site, and identifiers associated with your devices. This information enables Last List to
-        analyze how the Services are being accessed and used, and to track performance of the
-        Services. g. Location Information. Your devices (depending on your settings) may transmit
-        location information to the Services. Last List use this information to customize, improve
-        and protect the Services. For example, Last List may use your location information to
-        determine local language preferences, or to geotag a post. h. Cookies And Other
-        Technologies. Last List use these technologies to do things such as remember your
+        Last updated: Feburary 11th, 2018 1. This Privacy Policy describes what information Last
+        List, LLC (“Last List”) collects and how it is used and shared. Note that any capitalized
+        terms not defined in this Privacy Policy have the meanings set forth in Last List’s Terms of
+        Service. If you don’t agree with the terms of this Privacy Policy, you may not access or use
+        the Services. 2. Last List is committed to protecting and respecting your privacy. This
+        policy (sets out the basis on which any personal data Last List collect from you, or that
+        you provide to us, will be processed by us. Please read the following carefully to
+        understand Last List’s views and practices regarding your personal data and how Last List
+        will treat it. By visiting [www.mylastlist.com] (“Our Site”) you are accepting and
+        consenting to the practices described in this policy. 3. Last List will collect and process
+        the following data about you: a. Account Information. This may include your name, address,
+        email address and phone number. Last List use this information in the ways you would expect,
+        such as to set up your Account or contact you. b. Third-Party Account Information. If you
+        use Third-Party Services, you may provide Last List with your Third-Party Services account
+        information, such as your username (note that Last List will not store any passwords you use
+        to access Third-Party Services). Last List transmit, and may store, such account
+        information, only as needed to provide the Services, and only in accordance with the terms
+        and policies of the Third-Party Services. c. Payment Information. When you initially provide
+        or update your payment information, Last List transmit it via an encrypted connection to
+        Last List’s Payment Processor(s), which will uses and processes your payment information in
+        accordance with their policies Last List will not store your payment information, other than
+        your zip code and country, which Last List require for billing and to comply with tax and
+        other government regulations. d. Communications With Last List. When you send Last List
+        emails or other communications, such as customer support inquiries, Last List maintain those
+        communications and their contents so that Last List can resolve your inquiries or otherwise
+        assist you. e. Files You Provide Last List. When you use the Services, Last List store,
+        process and transmit Your Content. Last List may process and store such files and
+        information in order to provide the Services, as described in Last List’s Terms of Service
+        f. Usage Information. This includes information about your activity on and interaction with
+        the Services, such as your IP address, your device or browser type, the site you visited
+        before coming to Our Site, and identifiers associated with your devices. This information
+        enables Last List to analyze how the Services are being accessed and used, and to track
+        performance of the Services. g. Location Information. Your devices (depending on your
+        settings) may transmit location information to the Services. Last List use this information
+        to customize, improve and protect the Services. For example, Last List may use your location
+        information to determine local language preferences, or to geotag a post. h. Cookies And
+        Other Technologies. Last List use these technologies to do things such as remember your
         preferences, keep you safe and improve and promote the Services. You can read more about how
         Last List use these technologies in Last List’s Cookie Policy, which is incorporated by
         reference into this Privacy Policy. 4. You agree that Last List have the right to share your
@@ -110,7 +110,103 @@ const Policies = () => {
       </pre>
 
       <h4>Acceptable Use Policy</h4>
-      <pre />
+      <pre>
+        Last updated: Feburary 11th, 2018 1. This Acceptable Use Policy outlines some examples of
+        prohibited conduct in connection with Last List’s Services. Any capitalized terms not
+        defined in this Acceptable Use Policy have the meanings set forth in Terms of Service. 2. As
+        a Last List user, you agree not to misuse Our Site or the Services or help anyone else do
+        so. For example, you can't do any of the following in connection with Our Site or the
+        Services: a. You may and must not take apart, decompile or reverse engineer any part of the
+        Services in an effort to access things such as source code or algorithms nor access, search
+        or create accounts for the Services by any means other than Last List’s publicly supported
+        interfaces (for example, scraping, spidering or crawling). b. You may and must not take any
+        action that imposes an unreasonable load on Last List’s infrastructure or Last List’s
+        third-party providers. (Last List reserve the right to determine what’s reasonable.) c. You
+        may and must not provide your password to any other person, use any other person’s username
+        and password, or otherwise manage the Services through shared credentials. d. You may and
+        must not probe, scan or test the vulnerability of any system or network nor breach or
+        otherwise bypass any security or authentication measures nor access, tamper with or use
+        nonpublic areas or parts of the Services, or shared areas of the Services you haven't been
+        invited to, nor interfere with or disrupt any user, host or network (whether it’s Last
+        List’s or someone else’s), for example by distributing malware or overloading, flooding or
+        mail-bombing any part of the Services. e. You may and must not send spam or other
+        unsolicited communications, promotions or advertisements. (Last List reserve the right to
+        determine what constitutes spam) nor send altered, deceptive or false source-identifying
+        information, including, without limitation, phishing or spoofing. f. You may and must not do
+        anything that's false, fraudulent, inaccurate or deceiving nor impersonate another person,
+        company or entity. g. You may and must not engage in misleading or unethical marketing or
+        advertising nor use unauthorized credit cards, debit cards or other unauthorized payment
+        devices on the Service. h. You may and must not violate any person’s privacy or publicity
+        rights. i. You may and must not advocate bigotry or hatred against any person or group based
+        on their race, ethnicity, nationality, religion, gender, gender identity, sexual preference,
+        age or disability, nor do or post anything threatening, harassing, abusive, excessively
+        violent, offensive, sexually explicit or obscene. j. You may and must not promote or
+        advertise products or services other than your own without appropriate authorization. k. You
+        may and must not sell the Services unless specifically authorized to do so, or publish or
+        use the Services on any network or system other than those provided by Last List unless
+        previously authorized by Last List, nor infringe or misappropriate any person’s copyright,
+        trademark or other intellectual property rights. l. You may and must not use the Services in
+        violation of any US export controls, regulations or sanctions, nor do anything that
+        otherwise violates the law, including, without limitation, all local laws regarding online
+        conduct and acceptable content. 3. If Last List conclude that you’re violating any of these
+        policies, or engaging in any other behavior Last List deem abusive or inappropriate, Last
+        List may close down your Account. Last List reservse the right to remove any content or
+        suspend your Account, without any refund of any amounts paid for the Services, without
+        notice, at any time and for any reason. Last List reserve the right to enforce, or not
+        enforce, this Acceptable Use Policy in Last List’s sole discretion. 4. You can report
+        violations of these guidelines to Last List directly by emailing [EMAIL ADDRESS]. 5. Last
+        List may modify this Acceptable Use Policy from time to time, and will post the most current
+        version on Our Site.
+      </pre>
+
+      <h4>Cookie Policy</h4>
+      <pre>
+        1. This Cookie Policy describes how Last List, LLC (“Last List”) uses cookies and similar
+        technologies to provide, improve, promote and protect the Services. Note that any
+        capitalized terms not defined in this Cookie Policy have the meanings set forth in Last
+        List’s Terms of Service If you have any comments or questions about this Cookie Policy, feel
+        free to contact Last List [EMAIL ADDRESS]. 2. A cookie is a small file of letters and
+        numbers that Last List stores on your browser or the hard drive of your computer, if you
+        agree. Cookies contain information that is transferred to your computer's hard drive. 3.
+        Last List use the following cookies: a. Strictly necessary cookies. These are cookies that
+        are required for the operation of Last List’s website. They include, for example, cookies
+        that enable you to log into secure areas of Last List’s website or make use of e-billing
+        services. b. Analytical/performance cookies. They allow Last List to recognise and count the
+        number of visitors and to see how visitors move around Last List’s website when they are
+        using it. This helps Last List to improve the way Last List’s website works, for example, by
+        ensuring that users are finding what they are looking for easily. c. Functionality cookies.
+        These are used to recognise you when you return to Last List’s website. This enables Last
+        List to personalise Last List’s content for you, greet you by name and remember your
+        preferences (for example, your choice of language or region). d. Targeting cookies. These
+        cookies record your visit to Last List’s website, the pages you have visited and the links
+        you have followed. Last List will use this information to make Last List’s website and the
+        advertising displayed on it more relevant to your interests. Last List may also share this
+        information with third-parties for this purpose. e. Authentication, personalization and
+        security cookies. These help Last List verify your Account and device and determine when
+        you’re logged in, so Last List can make it easier for you to access the Services and provide
+        the appropriate experiences and features. Last List also use cookies to help prevent
+        fraudulent use of login credentials. f. Third-parties’ cookies. Third-party Services may use
+        cookies to help you sign into their services from Last List’s Services. Last List also may
+        use third-party cookies to assist with analyzing performance. Any third-party cookie usage
+        is governed by the privacy policy of the third-party placing the cookie. Please note that
+        third-parties (including, for example, advertising networks and providers of external
+        services like web traffic analysis services) may also use cookies over which Last List have
+        no control. These cookies are likely to be analytical/performance cookies or targeting
+        cookies g. Last List Ads’ cookies. Last List may partner with third-party publishers,
+        advertising networks and service providers to manage Last List’s ads on other sites. Last
+        List’s third-party partners may set cookies on your device or browser to gather information
+        about your activities on the Services and other sites you visit, in order to provide you
+        with Last List ads. For example, if you visit Last List and also use a social media
+        platform, you may see a Last List ad in your social media newsfeed or timeline. 4. A pixel
+        tag, or pixel, is a small piece of code that can be embedded on websites and emails. Last
+        List may use pixels to learn how you interact with Last List’s site pages and emails, and
+        this information helps Last List and Last List’s ad partners provide you with a more
+        tailored experience. 5. You block cookies by activating the setting on your browser that
+        allows you to refuse the setting of all or some cookies. However, if you use your browser
+        settings to block all cookies (including essential cookies) you may not be able to access
+        all or parts of Last List’s site. 6. Last List may modify this Cookie Policy from time to
+        time, and will post the most current version on Last List’s site.
+      </pre>
     </div>
   )
 }
