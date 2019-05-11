110aa2529c0c71a09235b00138a7bb0f100d0850
Grant Steinfeld <grantsteinfeld@gmail.com>
2019-05-11 13:04:08 +0000

    changed copy according to Mistress Lou

--- src/common/dictionaries/GuardiansDictionary.js
+++ src/common/dictionaries/GuardiansDictionary.js
@@ -5,8 +5,7 @@ export const guardians_dictionary = {
     main_text:
       '<p>\n' +
       '          You can choose up to five guardians of your Last List. They’ll be responsible for getting\n' +
-      '          it from us when you die. We’ll send them a message telling them all about it and cc you so\n' +
-      '          you’re in the loop.\n' +
+      '          it from us when you die. We’ll send them an email telling them all about it.' +
       '        </p>\n' +
       '        <p>\n' +
       '          You can change your guardians whenever you feel like it. We won’t share any of your or\n' +
